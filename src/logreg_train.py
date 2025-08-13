4f3c50fa718fc9a1afdb783013890f2cf98de4cc
Mathis <maujogue15@gmail.com>
2025-08-13 11:09:01 +0200

    [ADD] Train program

---
+++ src/logreg_train.py
@@ -0,0 +1,40 @@
+import argparse
+from tools.load import load
+from training.train import train_all
+from training.data_engineering import pre_process, split_train_validation
+
+if __name__ == "__main__":
+    try:
+        parser = argparse.ArgumentParser(
+            description="Train the logistic regression model"
+        )
+        parser.add_argument(
+            "file",
+            type=str,
+            help="The file to train the model on",
+            default="datasets/dataset_train.csv",
+            nargs="?",
+        )
+        parser.add_argument(
+            "--split",
+            type=float,
+            help="The split ratio for the training and validation sets",
+            const=0.2,
+            nargs="?",
+        )
+        args = parser.parse_args()
+
+        df = load(args.file)
+        if df is None:
+            exit(1)
+        print(args.split, "split")
+        if args.split is not None:
+            df_train, df_val = split_train_validation(df, args.split)
+        else:
+            df_train = df
+        X, Y_dict = pre_process(df_train)
+
+        train_all(X, Y_dict, 10000, 0.001)
+    except Exception as e:
+        print(f"Error: {e}")
+        exit(1)
---
+++ src/training/train.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def forward(X, w):
+    return sigmoid(np.matmul(X, w))
+
+
+# We don't use mse because it's not convex when used with sigmoid
+# There will be multiple local minima.
+# We use log loss because it's convex.
+def loss(X, Y, w):
+    y_hat = forward(X, w)
+    first_term = Y * np.log(y_hat)
+    second_term = (1 - Y) * np.log(1 - y_hat)
+    return -np.average(first_term + second_term)
+
+
+def gradient(X, Y, w):
+    return np.matmul(X.T, (forward(X, w) - Y)) / X.shape[0]
+
+
+def train(X, Y, iterations, lr):
+    w = np.zeros((X.shape[1], 1))
+    for i in range(iterations):
+        if i % 500 == 0:
+            print("Iteration %4d => Loss: %.20f" % (i, loss(X, Y, w)))
+        w_gradient = gradient(X, Y, w) * lr
+        w -= w_gradient
+    return w
+
+
+def train_all(X, Y_dict, iterations, lr):
+    w_gryffindor = train(X, Y_dict["Gryffindor"], iterations, lr)
+    w_hufflepuff = train(X, Y_dict["Hufflepuff"], iterations, lr)
+    w_ravenclaw = train(X, Y_dict["Ravenclaw"], iterations, lr)
+    w_slytherin = train(X, Y_dict["Slytherin"], iterations, lr)
+
+    with open("weights.txt", "w") as f:
+        f.write(f"Gryffindor:{w_gryffindor.flatten().tolist()}\n")
+        f.write(f"Hufflepuff:{w_hufflepuff.flatten().tolist()}\n")
+        f.write(f"Ravenclaw:{w_ravenclaw.flatten().tolist()}\n")
+        f.write(f"Slytherin:{w_slytherin.flatten().tolist()}")
